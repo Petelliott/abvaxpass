ba7f857cdbe6da41efd94180edf86f734b40bf21
Peter Elliott <pelliott@ualberta.ca>
2021-09-28 19:39:35 -0600

    added general skeleton of the app

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "sinatra", "~> 2.1"
+
+gem "jwt", "~> 2.2"
--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'jwt'
+
 
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+post '/register' do
+  payload = { birthDate: params[:birthdate],
+              doseMonth: params[:dosemonth],
+              doseYear: params[:doseyear],
+              phn: params[:phn] }
+  certificate = JWT.encode payload, nil, 'none'
+  redirect to("/certificate/#{certificate}"), 303
+end
+
+get '/certificate/:cert' do
+  erb :certificate, locals: { certificate: params[:cert] }
 end
