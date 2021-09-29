48bd613fb8931ce4490ccb326df610c8dffb74f0
Peter Elliott <pelliott@ualberta.ca>
2021-09-28 21:01:08 -0600

    added request to alberta health server and pdf parsing

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem "sinatra", "~> 2.1"
 
 gem "jwt", "~> 2.2"
+
+gem "pdf-reader", "~> 2.5"
--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'jwt'
-
+require 'net/http'
+require 'uri'
+require 'json'
+require 'pdf-reader'
+require 'stringio'
 
 get '/' do
   erb :index
@@ -11,10 +15,26 @@ post '/register' do
               doseMonth: params[:dosemonth],
               doseYear: params[:doseyear],
               phn: params[:phn] }
-  certificate = JWT.encode payload, nil, 'none'
+
+  result = Net::HTTP.post URI('https://northamerica-northeast1-alberta-covid-19-records.cloudfunctions.net/vaccine-immunization-card-generator'),
+                          payload.to_json,
+                          'Content-Type' => 'application/json'
+
+  passport = PDF::Reader.new(StringIO.new(result.body())).pages[0].text
+
+  cert_data = { firstname: /First Name: ([^\n]*)\n/.match(passport).captures[0],
+                lastname: /Last Name: ([^\n]*)\n/.match(passport).captures[0],
+                birthdate: /Birthdate:  ([^\n]*)\n/.match(passport).captures[0] }
+
+  certificate = JWT.encode cert_data, nil, 'none'
   redirect to("/certificate/#{certificate}"), 303
 end
 
 get '/certificate/:cert' do
-  erb :certificate, locals: { certificate: params[:cert] }
+  cert_data = JWT.decode(params[:cert], nil, false)[0]
+  p cert_data
+  erb :certificate, locals: { certificate: params[:cert],
+                              firstname: cert_data['firstname'],
+                              lastname: cert_data['lastname'],
+                              birthdate: cert_data['birthdate'] }
 end
